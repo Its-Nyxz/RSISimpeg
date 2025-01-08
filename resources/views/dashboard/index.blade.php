404d481a4a51a286c32acf82d3ec9600df2e9680
Its-Nyxz <ndemndem69@gmail.com>
2025-01-08 07:52:23 +0700

    update dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,16 @@
 <x-body>
-    <x-card title="Ini Dashboard" class="mb-2">
-        <div class="text-3xl font-semibold">{{ fake()->paragraph(50) }}</div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <x-card title="Total Karyawan" class="mb-2">
+            <div class="text-3xl font-semibold">{{ fake()->paragraph(5) }}</div>
+        </x-card>
+        <x-card title="Karyawan Hadir" class="mb-2">
+            <div class="text-3xl font-semibold">{{ fake()->paragraph(5) }}</div>
+        </x-card>
+        <x-card title="Karyawan Pulang" class="mb-2">
+            <div class="text-3xl font-semibold">{{ fake()->paragraph(5) }}</div>
+        </x-card>
+    </div>
+    <x-card title="Approval Cuti" class="mb-2">
+        <div class="text-3xl font-semibold">{{ fake()->paragraph(10) }}</div>
     </x-card>
 </x-body>
