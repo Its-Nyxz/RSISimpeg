b1eef8032ea2bd890225ae84cf268bbeb27b77b6
Its-Nyxz <ndemndem69@gmail.com>
2025-01-09 17:13:15 +0700

    update alpine index tunjangan

--- resources/views/tunjangan/index.blade.php
+++ resources/views/tunjangan/index.blade.php
@@ -1,66 +1,65 @@
 <x-body>
     <div>
         <div class="text-5xl font-regular mb-6 text-success-600 ">Master Data Tunjangan</div>
-        <div class="w-full bg-white border border-gray-200 rounded-lg shadow">
-            <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-success-3        00"
-                id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
-                {{-- @can('jabatan_view') --}}
+        <div x-data="{ activeTab: 'jabatan' }" class="w-full bg-white border border-gray-200 rounded-lg shadow">
+            <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-success-300"
+                role="tablist">
                 <li class="me-2">
-                    <button id="jabatan-tab" data-tabs-target="#jabatan" type="button" role="tab"
-                        aria-controls="jabatan" aria-selected="true"
-                        class="inline-block p-4 hover:text-white hover:bg-success-300 transition duration-200 ">Tunjangan
-                        Jabatan</button>
-                </li>
-                {{-- @endcan --}}
-                <li class="me-2">
-                    <button id="fungsional-tab" data-tabs-target="#fungsional" type="button" role="tab"
-                        aria-controls="fungsional" aria-selected="false"
-                        class="inline-block p-4 hover:text-white hover:bg-success-300 transition duration-200 ">Tunjangan
-                        Fungsional</button>
-                </li>
-                {{-- @can('trans_view') --}}
-                <li class="me-2">
-                    <button id="umum-tab" data-tabs-target="#umum" type="button" role="tab" aria-controls="umum"
-                        aria-selected="false"
-                        class="inline-block p-4 hover:text-white hover:bg-success-300 transition duration-200 ">Tunjangan
-                        Umum</button>
-                </li>
-                {{-- @endcan --}}
-                <li class="me-2">
-                    <button id="khusus-tab" data-tabs-target="#khusus" type="button" role="tab"
-                        aria-controls="khusus" aria-selected="false"
-                        class="inline-block p-4 hover:text-white hover:bg-success-300 transition duration-200 ">Tunjangan
-                        Khusus</button>
+                    <button @click="activeTab = 'jabatan'" :aria-selected="activeTab === 'jabatan'"
+                        class="inline-block p-4 transition duration-200 focus:outline-none"
+                        :class="activeTab === 'jabatan' ? 'bg-green-500 text-white' : 'hover:text-white hover:bg-success-300'">
+                        Tunjangan Jabatan
+                    </button>
                 </li>
                 <li class="me-2">
-                    <button id="tidak-tetap-tab" data-tabs-target="#tidak-tetap" type="button" role="tab"
-                        aria-controls="tidak-tetap" aria-selected="false"
-                        class="inline-block p-4 hover:text-white hover:bg-success-300 transition duration-200 ">Tunjangan
-                        Tidak Tetap</button>
+                    <button @click="activeTab = 'fungsional'" :aria-selected="activeTab === 'fungsional'"
+                        class="inline-block p-4 transition duration-200 focus:outline-none"
+                        :class="activeTab === 'fungsional' ? 'bg-green-500 text-white' : 'hover:text-white hover:bg-success-300'">
+                        Tunjangan Fungsional
+                    </button>
+                </li>
+                <li class="me-2">
+                    <button @click="activeTab = 'umum'" :aria-selected="activeTab === 'umum'"
+                        class="inline-block p-4 transition duration-200 focus:outline-none"
+                        :class="activeTab === 'umum' ? 'bg-green-500 text-white' : 'hover:text-white hover:bg-success-300'">
+                        Tunjangan Umum
+                    </button>
+                </li>
+                <li class="me-2">
+                    <button @click="activeTab = 'khusus'" :aria-selected="activeTab === 'khusus'"
+                        class="inline-block p-4 transition duration-200 focus:outline-none"
+                        :class="activeTab === 'khusus' ? 'bg-green-500 text-white' : 'hover:text-white hover:bg-success-300'">
+                        Tunjangan Khusus
+                    </button>
+                </li>
+                <li class="me-2">
+                    <button @click="activeTab = 'tidak-tetap'" :aria-selected="activeTab === 'tidak-tetap'"
+                        class="inline-block p-4 transition duration-200 focus:outline-none"
+                        :class="activeTab === 'tidak-tetap' ? 'bg-green-500 text-white' :
+                            'hover:text-white hover:bg-success-300'">
+                        Tunjangan Tidak Tetap
+                    </button>
                 </li>
             </ul>
-            <div id="defaultTabContent">
 
-                <div class="hidden p-4 bg-white rounded-lg md:p-8" id="jabatan" role="tabpanel">
+            <div id="defaultTabContent">
+                <div x-show="activeTab === 'jabatan'" class="p-4 bg-white rounded-lg md:p-8" role="tabpanel">
                     <livewire:data-tunjangan type="jabatan" />
                 </div>
-
-                <div class="hidden p-4 bg-white rounded-lg md:p-8" id="fungsional" role="tabpanel">
+                <div x-show="activeTab === 'fungsional'" class="p-4 bg-white rounded-lg md:p-8" role="tabpanel">
                     {{-- <livewire:asset-details type="fungsional" :aset="$aset" /> --}}
                 </div>
-                <div class="hidden p-4 bg-white rounded-lg md:p-8" id="umum" role="tabpanel">
+                <div x-show="activeTab === 'umum'" class="p-4 bg-white rounded-lg md:p-8" role="tabpanel">
                     {{-- <livewire:asset-details type="umum" :aset="$aset" /> --}}
                 </div>
-                <div class="hidden p-4 bg-white rounded-lg md:p-8" id="khusus" role="tabpanel">
+                <div x-show="activeTab === 'khusus'" class="p-4 bg-white rounded-lg md:p-8" role="tabpanel">
                     {{-- <livewire:asset-details type="tidak-tetap" :aset="$aset" /> --}}
                 </div>
-                <div class="hidden p-4 bg-white rounded-lg md:p-8" id="tidak-tetap" role="tabpanel">
+                <div x-show="activeTab === 'tidak-tetap'" class="p-4 bg-white rounded-lg md:p-8" role="tabpanel">
                     {{-- <livewire:asset-details type="tidak-tetap" :aset="$aset" /> --}}
                 </div>
             </div>
-
         </div>
 
-    </div>
-    {{-- <livewire:data-jabatan /> --}}
+        {{-- <livewire:data-jabatan /> --}}
 </x-body>
