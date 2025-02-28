7ac203e74752c84a5b9d942308696dbfa147dcdb
bgsmhrdka_ <bagasmahardikabudi2007@gmail.com>
2025-02-28 11:41:23 +0700

    active tab

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -6,3 +6,18 @@ if (!function_exists('rupiah')) {
         return 'Rp ' . number_format($angka, 0, ',', '.');
     }
 }
+
+if (!function_exists('isActiveMenu')) {
+    function isActiveMenu($items)
+    {
+        foreach ($items as $item) {
+            if (isset($item['href']) && request()->is(trim($item['href'], '/'))) {
+                return true;
+            }
+            if (isset($item['child']) && isActiveMenu($item['child'])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- resources/views/livewire/side-link.blade.php
+++ resources/views/livewire/side-link.blade.php
@@ -1,7 +1,11 @@
+@php
+    $isActive1 = isset($href) && request()->is(trim($href, '/')) ? 'bg-gray-100 text-success-950' : 'text-white';
+    $isOpen1 = isset($child) && isActiveMenu($this->child) ? 'block' : 'hidden';
+@endphp
 @if (isset($child) && empty($child))
     <li>
         <a href="{{ $href }}"
-            class="flex items-center p-2 pl-6 text-base font-medium text-white rounded-lg hover:bg-gray-100 transition duration-150 hover:text-success-950">
+            class="flex items-center p-2 pl-6 text-base font-medium rounded-lg hover:bg-gray-100 transition duration-150 hover:text-success-950 {{ $isActive1 }}">
             <i class="{{ $icon }}"></i>
             <span class="ml-3">{{ $title }}</span>
         </a>
@@ -9,28 +13,35 @@
 @else
     <li>
         <button type="button"
-            class="flex items-center p-2 pl-6 w-full text-base font-medium text-white hover:text-success-950 rounded-lg transition duration-150 group hover:bg-gray-100"
+            class="flex items-center p-2 pl-6 w-full text-base font-medium hover:text-success-950 rounded-lg transition duration-150 group hover:bg-gray-100 {{ $isActive1 }}"
             aria-controls="{{ Str::slug($title) }}" data-collapse-toggle="{{ Str::slug($title) }}">
             <i class="{{ $icon }}"></i>
             <span class="flex-1 ml-3 text-left whitespace-nowrap">{{ $title }}</span>
             <i class="fa-solid fa-chevron-down ml-2 group-hover:rotate-180 transition-transform"></i>
         </button>
-        <ul id="{{ Str::slug($title) }}" class="hidden py-2 space-y-2">
+        <ul id="{{ Str::slug($title) }}" class="py-2 space-y-2 {{ $isOpen1 }}">
             @foreach ($child as $item)
+                @php
+                    $isActive2 = isset($item['href']) && request()->is(trim($item['href'], '/')) ? 'bg-gray-100 text-success-950': 'text-white';
+                    $isOpen2 = isset($item['child']) && isActiveMenu($item['child']) ? 'block' : 'hidden';
+                @endphp
                 <li>
                     @if (!empty($item['child']))
                         <button type="button"
-                            class="flex items-center p-2 pl-10 w-full text-base font-medium text-white rounded-lg transition duration-150 group hover:bg-gray-100 hover:text-success-950"
+                            class="flex items-center p-2 pl-10 w-full text-base font-medium rounded-lg transition duration-150 group hover:bg-gray-100 hover:text-success-950 {{ $isActive2 }}"
                             aria-controls="{{ Str::slug($item['title']) }}"
                             data-collapse-toggle="{{ Str::slug($item['title']) }}">
                             <span class="flex-1 text-left whitespace-nowrap">{{ $item['title'] }}</span>
                             <i class="fa-solid fa-chevron-down ml-2 group-hover:rotate-180 transition-transform"></i>
                         </button>
-                        <ul id="{{ Str::slug($item['title']) }}" class="hidden py-2 space-y-2">
+                        <ul id="{{ Str::slug($item['title']) }}" class="py-2 space-y-2 {{ $isOpen2 }}">
                             @foreach ($item['child'] as $subItem)
+                                @php
+                                    $isActive3 = isset($subItem['href']) && request()->is(trim($subItem['href'], '/')) ? 'bg-gray-100 text-success-950' : 'text-white';
+                                @endphp
                                 <li>
                                     <a href="{{ $subItem['href'] }}"
-                                        class="flex text-left p-2 pl-12 text-base font-medium text-white rounded-lg hover:bg-gray-100 transition duration-150 hover:text-success-950">
+                                        class="flex text-left p-2 pl-12 text-base font-medium rounded-lg hover:bg-gray-100 transition duration-150 hover:text-success-950 {{ $isActive3 }}">
                                         {{ $subItem['title'] }}
                                     </a>
                                 </li>
@@ -38,7 +49,7 @@
                         </ul>
                     @else
                         <a href="{{ $item['href'] }}"
-                            class="flex items-center p-2 pl-10 w-full text-base font-medium text-white rounded-lg transition duration-150 group hover:bg-gray-100 hover:text-success-950">
+                            class="flex items-center p-2 pl-10 w-full text-base font-medium rounded-lg transition duration-150 group hover:bg-gray-100 hover:text-success-950 {{ $isActive2 }}">
                             <span class="flex-1 text-left whitespace-nowrap">{{ $item['title'] }}</span>
                         </a>
                     @endif
