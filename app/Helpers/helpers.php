7627ecac0556f02b9cb1e13439ccccea1a8183f0
Orvillian <Novipancaangkrok@gmail.com>
2025-03-03 05:26:07 +0700

    active menu

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -5,4 +5,23 @@ if (!function_exists('rupiah')) {
     {
         return 'Rp ' . number_format($angka, 0, ',', '.');
     }
+
+    if (!function_exists('isActiveMenu')) {
+        function isActiveMenu($items)
+        {
+            foreach ($items as $item) {
+
+
+                if (isset($item['href']) && request()->is(trim($item['href'], '/'))) {
+
+
+                    return true;
+                }
+                if (isset($item['child']) && isActiveMenu($item['child'])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
