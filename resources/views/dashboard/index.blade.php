ab8304e82e30472140b7fe667d05639e7a220013
Its-Nyxz <ndemndem69@gmail.com>
2025-03-19 07:01:52 +0700

    update blade dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,5 @@
 <x-body>
-    <h1 class="text-2xl font-bold text-success-900 mb-4">Dashboard</h1>
+    <h1 class="text-2xl font-bold text-success-900 mb-4">Dashboard {{ Auth::user()->unitKerja->nama ?? ' ' }}</h1>
     {{-- <livewire:data-absen type="absen" /> --}}
     {{-- <x-card-tanpa-title class="max-w-md">
         <div class="flex flex-col" style="margin-left: 30px;">
@@ -63,47 +63,50 @@
             </x-card>
         </div>
 
-        <!-- Kolom Kanan -->
-        <div class="md:col-span-1">
-            <x-card title="Total Karyawan" class="mb-4">
-                <div class="flex flex-col items-left" style="margin-left: 30px;">
-                    <div class="flex items-left gap-6 mb-4">
-                        <div class="flex items-center justify-center w-12 h-12 rounded-full"
-                            style="background-color: #3C986A;">
-                            <i class="fa-solid fa-people-group" style="color: white; font-size: 24px;"></i>
+        @if (Auth::user()->unitKerja->nama == 'KEPEGAWAIAN')
+            <!-- Kolom Kanan -->
+            <div class="md:col-span-1">
+                <x-card title="Total Karyawan" class="mb-4">
+                    <div class="flex flex-col items-left" style="margin-left: 30px;">
+                        <div class="flex items-left gap-6 mb-4">
+                            <div class="flex items-center justify-center w-12 h-12 rounded-full"
+                                style="background-color: #3C986A;">
+                                <i class="fa-solid fa-people-group" style="color: white; font-size: 24px;"></i>
+                            </div>
+                            <h2 class="text-2xl font-semibold" style="color: #3C986A; margin-top:7px;">Total Karyawan
+                            </h2>
                         </div>
-                        <h2 class="text-2xl font-semibold" style="color: #3C986A; margin-top:7px;">Total Karyawan</h2>
-                    </div>
-                    <div class="flex items-center gap-6" style="margin-top: 10px; margin-bottom: 10px;">
-                        {{-- <div class="text-center">
+                        <div class="flex items-center gap-6" style="margin-top: 10px; margin-bottom: 10px;">
+                            {{-- <div class="text-center">
                             <div class="text-3xl font-semibold" style="color: #3C986A;">{{ $totalKaryawanAktif }}</div>
                             <span class="badge rounded-lg"
                                 style="color: white; background-color:#3C986A; padding-left: 12.5px; padding-right: 12.5px;">
                                 Aktif</span>
                         </div> --}}
-                        @foreach ($jumlahKaryawan as $karyawan)
-                            <div class="text-center">
-                                <div class="text-3xl font-semibold" style="color: #3C986A;">
-                                    {{ $karyawan->total }}
+                            @foreach ($jumlahKaryawan as $karyawan)
+                                <div class="text-center">
+                                    <div class="text-3xl font-semibold" style="color: #3C986A;">
+                                        {{ $karyawan->total }}
+                                    </div>
+                                    <span class="badge rounded-lg"
+                                        style="color: white; background-color:#3C986A; padding-left: 12.5px; padding-right: 12.5px;">
+                                        {{ $karyawan->nama }}
+                                    </span>
                                 </div>
-                                <span class="badge rounded-lg"
-                                    style="color: white; background-color:#3C986A; padding-left: 12.5px; padding-right: 12.5px;">
-                                    {{ $karyawan->nama }}
-                                </span>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
+                        <div class="mt-4 text-left">
+                            <a href="/datakaryawan"
+                                class="inline-flex items-center px-4 py-2 text-white rounded-lg hover:bg-green-700"
+                                style="background-color: #3C986A; border-radius: 100px; ">
+                                Lihat
+                                <i class="fa-solid fa-circle-chevron-right ml-2" style="color: #ffffff;"></i>
+                            </a>
+                        </div>
                     </div>
-                    <div class="mt-4 text-left">
-                        <a href="/datakaryawan"
-                            class="inline-flex items-center px-4 py-2 text-white rounded-lg hover:bg-green-700"
-                            style="background-color: #3C986A; border-radius: 100px; ">
-                            Lihat
-                            <i class="fa-solid fa-circle-chevron-right ml-2" style="color: #ffffff;"></i>
-                        </a>
-                    </div>
-                </div>
-            </x-card>
-        </div>
+                </x-card>
+            </div>
+        @endif
     </div>
     <div>
         <x-card title="Jadwal Absen">
