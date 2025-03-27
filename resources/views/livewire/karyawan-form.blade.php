09763b07dc879b152d78d8ac889cacfe352bf836
bgsmhrdka_ <bagasmahardikabudi2007@gmail.com>
2025-03-27 11:34:07 +0700

    bug di create karyawan

--- resources/views/livewire/karyawan-form.blade.php
+++ resources/views/livewire/karyawan-form.blade.php
@@ -481,7 +481,7 @@
                                 <select id="khusus" wire:model.live="khusus"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5">
                                     <option value="">Pilih Tunjangan Khusus</option>
-                                    @foreach ($khususList as $khus)
+                                    @foreach ($filteredKhusus as $khus)
                                         <option value="{{ $khus->id }}">
                                             {{ $khus->nama }}
                                         </option>
