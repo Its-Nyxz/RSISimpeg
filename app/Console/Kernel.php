3009c260fb9ae9fd70a6b2cde95b7e2b5f37071f
Its-Nyxz <ndemndem69@gmail.com>
2025-08-11 10:58:50 +0700

    update generate back to old and kernel generate to hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends ConsoleKernel
             ->dailyAt('00:30')
             ->timezone('Asia/Jakarta');
 
-        $schedule->command('generate:absen-timeout')->everyTenMinutes()->timezone('Asia/Jakarta');
+        $schedule->command('generate:absen-timeout')->hourly()->timezone('Asia/Jakarta');
     }
 
     /**
