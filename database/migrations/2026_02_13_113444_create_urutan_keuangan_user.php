8a86d55253d5dbee5d4e96f4c1784126a7fe2a46
Lenovo <orvinoray@gmail.com>
2026-02-17 14:44:13 +0700

    membuat migrasi urutan keuangan

--- database/migrations/2026_02_13_113444_create_urutan_keuangan_user.php
+++ database/migrations/2026_02_13_113444_create_urutan_keuangan_user.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('urutan_keuangan_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
+            $table->integer('urutan');
             $table->timestamps();
         });
     }
